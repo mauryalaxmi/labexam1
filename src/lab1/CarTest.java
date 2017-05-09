3f0ab56acf60c435131c256bc08faf77a967a9bf
laxmi <laxshu1516@gmail.com>
2017-05-09 11:56:20 +0530

    third commit
    Signed-off-by: laxmi <laxshu1516@gmail.com>

---
+++ src/lab1/CarTest.java
@@ -0,0 +1,27 @@
+package lab1;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class CarTest {
+	Car car1;
+	@Before
+	public void setUp() throws Exception {
+		Car car1=new Car(100);
+	}
+
+	@Test
+	public void testGetengineCapcity() {
+		//fail("Not yet implemented");
+		
+	}
+
+	@Test
+	public void testGetEngineCapcity() {
+		//fail("Not yet implemented");
+		
+	}
+
+}
